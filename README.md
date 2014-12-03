863f170d749daeabdb948939743ede203fefdf90
sighmon <simon.loffler@gmail.com>
2014-12-04 00:06:13 +1030

    First tests with the reader.

---
+++ README.md
@@ -0,0 +1,19 @@
+Key Master
+==========
+An Arduino based RFID/NFC door access system that sends users welcome information to LED signs in the hackerspace, as well as pinging the future hackerspace website to count user visits to the space.
+
+Features
+--------
+* Use any RFID/NFC device to check-in to a hack session.
+* Sends a welcome message to an LED display.
+* POSTs info to the [hackadl.org](http://hackadl.org) website.
+
+Hardware
+--------
+* ACR122 Contactless Reader
+
+Software
+--------
+* Python
+
+[Read more on the wiki](http://hackerspace-adelaide.org.au/wiki/Key_Master)
---
+++ keymaster.py
@@ -0,0 +1,58 @@
+#######################################################
+## Key Master - Hackerspace Adelaide NFC Door reader ##
+#######################################################
+
+# scard documentation:
+# http://pyscard.sourceforge.net/epydoc/smartcard.scard.scard-module.html
+
+# TODO: Post successful card scan to hackadl.org
+
+import urllib
+import urllib2
+
+from smartcard.scard import *
+
+hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+
+assert hresult==SCARD_S_SUCCESS
+
+hresult, readers = SCardListReaders(hcontext, [])
+
+assert len(readers)>0
+
+reader = readers[0]
+
+print '\n###### Hackadl.org.au ######'
+print '######    Oh hello.   ######\n'
+
+readerstates = []
+for i in xrange(len(readers)):
+    readerstates += [ (readers[i], SCARD_STATE_UNAWARE) ]
+hresult, newstates = SCardGetStatusChange(hcontext, 0, readerstates)
+
+while True:
+  hresult, newstates = SCardGetStatusChange(hcontext, INFINITE, newstates)
+  for reader, eventstate, atr in newstates:
+    if eventstate & SCARD_STATE_PRESENT:
+      print 'Card found'
+      hresult, hcard, dwActiveProtocol = SCardConnect(
+      hcontext,
+      reader,
+      SCARD_SHARE_SHARED,
+      SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
+      hresult, reader, state, protocol, atr = SCardStatus(hcard)
+      print 'ATR:', atr
+      hresult, response = SCardTransmit(hcard,dwActiveProtocol,[0xFF,0xCA,0x00,0x00,0x00])
+      print 'ID:', response, '\n'
+
+      # TODO: POST the card data
+      # url = 'http://hackadl.org/checkin'
+      # data = urllib.urlencode({'atr' : atr, 'id' : response})
+      # content = urllib2.urlopen(url, data).read()
+      # print content
+
+    elif eventstate & SCARD_STATE_EMPTY:
+      print 'Reader empty\n'
+
+    else:
+      print 'Unknown event state', eventstate
